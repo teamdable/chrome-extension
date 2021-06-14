ead0508d764897a39b23e4bd48cb44bfa503889c
goonoo <tenshi@dable.io>
2021-06-14 10:38:40 +0900

    Manifest V3

--- src/bg/popup.js
+++ src/bg/popup.js
@@ -1,60 +1,95 @@
 /* eslint-disable */
+
+function getWidgetsHtml(widgets) {
+  let t = `
+<table>
+  <thead>
+    <tr>
+      <th>WidgetID</th>
+      <th>WidgetURL</th>
+      <th>Height</th>
+      <th>Dashboard</th>
+    </tr>
+  </head>
+  <tbody>
+`;
+  if (widgets.length > 0) {
+    for (var i=0; i<widgets.length; i++) {
+      const w = widgets[i];
+      t += `
+        <tr>
+          <td>
+      `;
+      if (w.is_top) {
+        t += `
+        <a href="#" class="widget_id"
+          data-widget_id="${w.widget_id}"
+          data-org_url="${w.org_url}"
+          data-is_top="${w.is_top}"
+        >${w.widget_id}</a>
+        `;
+      } else {
+        t += `
+        ${w.widget_id}<br />
+        (in frame)
+        `;
+      }
+      t += `
+          </td>
+          <td><a href="${w.debug_url}" class="url" target="_blank">
+              ${w.debug_url}</a></td>
+          <td>${w.height}px</td>
+          <td><a href="https://admin.dable.io/redirect/widgets/${w.widget_id}" target="_blank">Go</a></td>
+        </tr>
+      `;
+    }
+  } else {
+    t += `
+      <tr>
+        <td colspan="4" style="text-align:center">No widget installed.</td>
+      </tr>
+    `;
+  }
+  t += `
+  </tbody>
+</table>
+  `;
+  return t;
+}
+
 function popup() {
   chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
-    var activeTab = tabs[0];
-    var tabId = activeTab.id;
-    chrome.webNavigation.getAllFrames({ tabId: tabId }, function(details) {
-      var widgets = [];
-      var urls = [];
-      var resp_count = 0;
+    const activeTab = tabs[0];
+    const tabId = activeTab.id;
 
-      details.sort( (a, b) => a.frameId - b.frameId );
-      var uniq_pages = details.filter( (d) => {
-        if (urls.indexOf(d.url) > -1) return false;
-        urls.push(d.url);
-        return true;
-      });
-
-      var frame_count = details.length;
-
-      uniq_pages.forEach(function (d) {
-        chrome.tabs.sendMessage(tabId, {
-          "type": "widget",
-          url: d.url,
-        }, function (resp) {
-          resp_count++;
-
-          if (resp && resp.widgets.length > 0) {
-            resp.widgets.forEach(function (w) {
-              w.is_top = d.parentFrameId === -1;
-              if (w.url) {
-                w.debug_url = w.url + (w.url.indexOf('?') > -1 ? '&' : '?') + '&debug=1';
-              } else {
-                w.url = '';
-                w.debug_url = '';
-              }
-            });
-            widgets = widgets.concat(resp.widgets);
-          }
-
-          if (frame_count === details.length) {
-            var el = document.getElementById("widgets");
-            el.innerHTML = tmpl("widgets_tmpl", { widgets: widgets });
-
-            var elements = document.getElementsByClassName("widget_id");
-            for (var i=0; i<elements.length; i++) {
-              elements[i].addEventListener('click', function (e) {
-                e.preventDefault();
-                focusWidget(
-                  this.getAttribute('data-widget_id'),
-                  this.getAttribute('data-org_url')
-                );
-              });
-            }
+    chrome.runtime.sendMessage(
+      "ghpnefibglfjlhalajogommaabfohjhj",
+      { request_widget_list: true, tabId },
+      function ({ widget_list }) {
+        widget_list.forEach(function (w) {
+          if (w.url) {
+            w.debug_url = w.url + (w.url.indexOf('?') > -1 ? '&' : '?') + '&debug=1';
+          } else {
+            w.url = '';
+            w.debug_url = '';
           }
         });
-      });
-    });
+
+        const el = document.getElementById("widgets");
+        el.innerHTML = getWidgetsHtml(widget_list);
+
+        const elements = document.getElementsByClassName("widget_id");
+        for (let i=0; i<elements.length; i++) {
+          elements[i].addEventListener('click', function (e) {
+            e.preventDefault();
+            focusWidget(
+              this.getAttribute('data-widget_id'),
+              this.getAttribute('data-org_url')
+            );
+          });
+        }
+      }
+    );
   });
 }
 
@@ -65,7 +100,6 @@ function focusWidget(widget_id, org_url) {
       "type": "focus_widget",
       "widget_id": widget_id,
       "url": org_url,
-    }, function (resp) {
     });
   });
 }
--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,32 +1,64 @@
 /* eslint-disable */
+
+function onLoadPage() {
+  const widget_els = document.querySelectorAll('[data-widget_id]');
+  if (widget_els.length === 0) return;
+
+  const widget_count = widget_els.length;
+  const widget_list = [];
+  let widget_checked_els = 0;
+
+  const done = function () {
+    chrome.runtime.sendMessage(
+      "ghpnefibglfjlhalajogommaabfohjhj",
+      { widget_count, widget_list }
+    );
+  };
+
+  const checkWidgetEl = function (el) {
+    const f = el.getElementsByTagName('iframe')[0];
+    const is_loading = !!el.getElementsByTagName('script')[0];
+    if (f) {
+      widget_list.push( {
+        widget_id: el.getAttribute('data-widget_id'),
+        url: f.getAttribute('src') || f.getAttribute('data-org_src'),
+        height: f.height,
+        org_url: window.location.href,
+        is_top: window.top === window,
+      } );
+      widget_checked_els++;
+    } else if (is_loading) {
+      setTimeout(() => {
+        checkWidgetEl(el);
+      }, 500);
+    } else {
+      widget_checked_els++;
+    }
+
+    if (widget_checked_els === widget_count) done();
+  };
+
+  widget_els.forEach(function (el) {
+    checkWidgetEl(el);
+  });
+}
+
 chrome.runtime.onMessage.addListener(
   function(message, sender, sendResponse) {
-    if (message.type === 'widget' && message.url === window.location.href) {
-      var widget_list = [];
-      var $widgets = $('[data-widget_id]');
-      $widgets.each(function () {
-        widget_list.push( {
-          widget_id: $(this).attr('data-widget_id'),
-          url: $(this).find('iframe').attr('src') || $(this).find('iframe').attr('data-org_src'),
-          height: $(this).find('iframe').height(),
-          org_url: window.location.href,
-        } );
-      });
-      sendResponse({
-        widgets: widget_list
-      });
-    } else if (message.type === 'focus_widget' && message.url === window.location.href) {
+    if (message.type === 'focus_widget' && message.url === window.location.href) {
       var widget_id = message.widget_id;
-      var $widget = $('[data-widget_id="' + widget_id + '"]');
-      window.scrollTo(0, $widget.offset().top - 100);
+      var widget_el = document.querySelector('[data-widget_id="' + widget_id + '"]');
+      if (!widget_el) return;
+      var offset_top = widget_el.getBoundingClientRect().top + window.pageYOffset;
+      window.scrollTo(0, offset_top - 100);
 
       // focus
       var cnt = 0;
       var interval_id = setInterval(function () {
         if (cnt % 2 === 0) {
-          $widget.css('outline', 'solid 5px red');
+          widget_el.style.outline = 'solid 5px red';
         } else {
-          $widget.css('outline', '');
+          widget_el.style.outline = '';
         }
         cnt++;
         if (cnt > 9) clearInterval(interval_id);
@@ -34,3 +66,10 @@ chrome.runtime.onMessage.addListener(
     }
   }
 );
+
+if (document.readyState === 'complete')
+  onLoadPage();
+else
+  window.addEventListener("load", function() {
+    setTimeout(onLoadPage, 0);
+  });
